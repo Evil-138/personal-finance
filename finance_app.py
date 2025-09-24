59c5fdb0d44f4c181955343fc1d514fd01ca31fe
PRIYANSHU <128232137+Evil-138@users.noreply.github.com>
2025-09-24 13:22:13 +0530

    Update finance_app.py

--- finance_app.py
+++ finance_app.py
@@ -367,4 +367,7 @@ def recommend():
     return jsonify({'text': text})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
