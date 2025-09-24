90340f31cbcf2ace2f6f95acd704032fd913a1e2
PRIYANSHU <128232137+Evil-138@users.noreply.github.com>
2025-09-24 16:35:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -367,7 +367,8 @@ def recommend():
     return jsonify({'text': text})
 
 if __name__ == "__main__":
-  import os
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
